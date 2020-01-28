4f8875c71bab1ddc109f52ba9922622fcdfd8816
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2020-01-28 12:44:37 -0600

    Avance 2

--- app/src/main/java/com/example/accelerometerexample/MainActivity.java
+++ app/src/main/java/com/example/accelerometerexample/MainActivity.java
@@ -2,22 +2,28 @@ package com.example.accelerometerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private Sensor sensor;
     private SensorManager manager;
+    private float lastX;
+    private float lastY;
 
     TextView xLabel;
     TextView yLabel;
     TextView zLabel;
+    ImageView hamster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +33,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         xLabel = findViewById(R.id.x);
         yLabel = findViewById(R.id.y);
         zLabel = findViewById(R.id.z);
+        hamster = findViewById(R.id.hamster);
 
         manager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        lastX = 0;
+        lastY = 0;
     }
 
     @Override
@@ -50,9 +60,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event){
+        float deltaX = lastX - event.values[0];
+        float deltaY = lastY - event.values[1];
+
         xLabel.setText(Float.toString(event.values[0]));
         yLabel.setText(Float.toString(event.values[1]));
         zLabel.setText(Float.toString(event.values[2]));
+        lastX = event.values[0];
+        lastY = event.values[1];
+        ObjectAnimator.ofFloat(hamster, "translationX", deltaX).start();
+        ObjectAnimator.ofFloat(hamster, "translationY", deltaY).start();
     }
 
     @Override
