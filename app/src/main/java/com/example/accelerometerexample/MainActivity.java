c01b5a9265d7d0f590d3e0077dbdbe42bd39c3f2
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2020-01-28 12:32:16 -0600

    Avance

--- app/src/main/java/com/example/accelerometerexample/MainActivity.java
+++ app/src/main/java/com/example/accelerometerexample/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.accelerometerexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Sensor sensor;
+    private SensorManager manager;
+
+    TextView xLabel;
+    TextView yLabel;
+    TextView zLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        xLabel = findViewById(R.id.x);
+        yLabel = findViewById(R.id.y);
+        zLabel = findViewById(R.id.z);
+
+        manager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(sensor != null){
+            manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(sensor != null){
+            manager.unregisterListener(this, sensor);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        xLabel.setText(Float.toString(event.values[0]));
+        yLabel.setText(Float.toString(event.values[1]));
+        zLabel.setText(Float.toString(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
